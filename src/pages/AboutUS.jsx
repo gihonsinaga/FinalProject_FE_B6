13edacbf103f9319dc38c18f881150c398ccf41a
RafiDhafinErsamazaya <160104105+RafiDhafinErsamazaya@users.noreply.github.com>
2024-07-01 16:20:25 +0700

    update about-us dan konsisten bahasa

--- src/pages/AboutUS.jsx
+++ src/pages/AboutUS.jsx
@@ -21,77 +21,115 @@ export default function AboutUS() {
                     </div>
                 </section>
 
-                <section className="mb-20">
+                <section className="mb-16">
                     <h2 className="text-2xl font-semibold mb-6">Nilai Kami</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                        <div className="bg-white cursor-pointer border border-blue-700 rounded-lg shadow-lg p-8 lg:p-14 transform transition duration-500 hover:scale-105">
-                            <h3 className="text-xl font-bold text-blue-700 mb-3">Kemudahan</h3>
-                            <p className="text-gray-700 text-sm">Kami memastikan proses pemesanan tiket pesawat yang cepat, mudah, dan aman untuk memberikan kenyamanan terbaik bagi pelanggan kami.</p>
-                        </div>
-                        <div className="bg-white cursor-pointer border border-blue-700 rounded-lg shadow-lg p-8 lg:p-14 transform transition duration-500 hover:scale-105">
-                            <h3 className="text-xl font-bold text-blue-700 mb-3">Inovasi</h3>
-                            <p className="text-gray-700 text-sm">Kami terus berinovasi dalam mengembangkan solusi pemesanan yang cerdas dan ramah lingkungan untuk perjalanan yang lebih baik.</p>
-                        </div>
-                        <div className="bg-white cursor-pointer border border-blue-700 rounded-lg shadow-lg p-8 lg:p-14 transform transition duration-500 hover:scale-105">
-                            <h3 className="text-xl font-bold text-blue-700 mb-3">Pelayanan</h3>
-                            <p className="text-gray-700 text-sm">Kami berkomitmen untuk memberikan pelayanan terbaik dengan mendengarkan dan memahami kebutuhan setiap pelanggan kami.</p>
-                        </div>
-                    </div>
+                    <div className="grid grid-cols-3 gap-5 max-sm:grid-cols-1 max-md:grid-cols-2 max-sm:text-sm ">
+            <div className="border-2 rounded-3xl sm:px-10 sm:py-7 max-sm:px-3 max-sm:py-4  border-slate-400 max-sm:text-center">
+              <div>
+                <img
+                  src="/assets/WhyUs1.png"
+                  width="60px"
+                  height="65px"
+                  alt=""
+                  className="max-sm:w-[40px] max-sm:mx-auto"
+                />
+              </div>
+              <div className="font-bold sm:mt-7 max-sm:mt-2 mb-1 sm:text-base text-black tracking-wide max-sm:text-sm">
+                Terjangkau
+              </div>
+              <div className="sm:text-sm text-black max-sm:text-xs">
+                Kami menyediakan berbagai macam harga, termasuk yang paling
+                murah
+              </div>
+            </div>
+
+            <div className="border-2 rounded-3xl sm:px-10 sm:py-7 max-sm:px-3 max-sm:py-4 border-slate-400 max-sm:text-center">
+              <div>
+                <img
+                  src="/assets/WhyUs2.png"
+                  width="60px"
+                  height="65px"
+                  alt=""
+                  className="max-sm:w-[40px] max-sm:mx-auto"
+                />
+              </div>
+              <div className="font-bold sm:mt-7 max-sm:mt-2 mb-1 sm:text-base text-black tracking-wide max-sm:text-sm">
+                Efisien
+              </div>
+              <div className="sm:text-sm text-black max-sm:text-xs">
+                Kami dapat mengantarkan kebutuhan Anda lebih cepat{" "}
+              </div>
+            </div>
+
+            <div className="border-2 rounded-3xl sm:px-10 sm:py-7 max-sm:px-3 max-sm:py-4 border-slate-400 max-sm:text-center">
+              <div>
+                <img
+                  src="/assets/WhyUs3.png"
+                  width="60px"
+                  height="65px"
+                  alt=""
+                  className="max-sm:w-[40px] max-sm:mx-auto"
+                />
+              </div>
+              <div className="font-bold sm:mt-7 max-sm:mt-2 mb-1 sm:text-base text-black tracking-wide max-sm:text-sm">
+                Kesetiaan pelanggan
+              </div>
+              <div className="sm:text-sm text-black max-sm:text-xs">
+                Selalu memberikan layanan #1 untuk pelanggan{" "}
+              </div>
+            </div>
+          </div>
                 </section>
 
                 <section>
-                    <h2 className="text-2xl font-semibold mb-6">Tim Kami</h2>
-                    <div className="bg-white rounded-xl p-6 sm:p-12 lg:px-24 lg:py-12 my-12">
-                        <div className="mb-12 font-bold text-center text-xl">
-                            Front End
-                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+                    {/* <h2 className="text-2xl font-semibold mb-2">Tim Kami</h2> */}
+                    <div className="bg-white rounded-xl p-20 md:px-16 lg:px-32 lg:py-16 my-12">
+                        <div className="mb-12 font-bold text text-2xl">
+                           FlyNow Team
+                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-8">
                                 <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
-                                    <img src={loginPict} alt="Anggota Tim 1" className="w-full h-48 object-cover"/>
+                                    <img src="src/assets/gihon.jpg" alt="Anggota Tim 1" className="w-full h-48 object-cover"/>
                                     <div className="p-6">
-                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-2">Nama Anggota 1</h3>
-                                        <p className="text-xl text-gray-600">CEO</p>
+                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-1">Gihon Sinaga</h3>
+                                        <p className="text-sm text-gray-600">Front End Developer</p>
+                                    </div>
+                                </div>
+                                <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
+                                    <img src="src/assets/zanneta.png" alt="Anggota Tim 2" className="w-full h-48 object-cover"/>
+                                    <div className="p-6">
+                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-1">Zanetta Aisha D</h3>
+                                        <p className="text-sm text-gray-600">Front End Developer</p>
+                                    </div>
+                                </div>
+                                <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
+                                    <img src="src/assets/rafi.jpg" alt="Anggota Tim 3" className="w-full h-48 object-cover"/>
+                                    <div className="p-6">
+                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-1">Rafi Dhafin E</h3>
+                                        <p className="text-sm text-gray-600">Front End Developer</p>
+                                    </div>
+                                </div>
+                                <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
+                                    <img src="src/assets/bagus.png" alt="Anggota Tim 1" className="w-full h-48 object-cover"/>
+                                    <div className="p-6">
+                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-1">Bagus Dwi Putra A</h3>
+                                        <p className="text-sm text-gray-600">Back End Developer</p>
                                     </div>
                                 </div>
                                 <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
                                     <img src={loginPict} alt="Anggota Tim 2" className="w-full h-48 object-cover"/>
                                     <div className="p-6">
-                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-2">Nama Anggota 2</h3>
-                                        <p className="text-xl text-gray-600">CTO</p>
+                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-1">Akhdan Robbani</h3>
+                                        <p className="text-sm text-gray-600">Back End Developer</p>
                                     </div>
                                 </div>
                                 <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
-                                    <img src={loginPict} alt="Anggota Tim 3" className="w-full h-48 object-cover"/>
+                                    <img src="src/assets/shakti.jpg" alt="Anggota Tim 3" className="w-full h-48 object-cover"/>
                                     <div className="p-6">
-                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-2">Nama Anggota 3</h3>
-                                        <p className="text-xl text-gray-600">CFO</p>
+                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-2">Lanang Shakti P</h3>
+                                        <p className="text-sm text-gray-600">Back End Developer</p>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="mb-12 font-bold text-center text-xl">
-                            Back End
-                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
-                                <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
-                                    <img src={loginPict} alt="Anggota Tim 1" className="w-full h-48 object-cover"/>
-                                    <div className="p-6">
-                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-2">Nama Anggota 1</h3>
-                                        <p className="text-xl text-gray-600">CEO</p>
-                                    </div>
-                                </div>
-                                <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
-                                    <img src={loginPict} alt="Anggota Tim 2" className="w-full h-48 object-cover"/>
-                                    <div className="p-6">
-                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-2">Nama Anggota 2</h3>
-                                        <p className="text-xl text-gray-600">CTO</p>
-                                    </div>
-                                </div>
-                                <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
-                                    <img src={loginPict} alt="Anggota Tim 3" className="w-full h-48 object-cover"/>
-                                    <div className="p-6">
-                                        <h3 className="text-xl font-semibold text-secondaryBlue mb-2">Nama Anggota 3</h3>
-                                        <p className="text-xl text-gray-600">CFO</p>
-                                    </div>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
